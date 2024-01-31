b6a764012ae0caa7034a93f90c64c5076c20f006
sriram98v <sriram.98v@gmail.com>
2024-01-30 18:21:27 -0600

    removed default implementations temporarily

--- src/tree/ops.rs
+++ src/tree/ops.rs
@@ -1,22 +1,22 @@
 use crate::tree::simple_rtree::SimpleRootedTree;
-use std::rc::Rc;
 
 pub trait SPR
 where
     Self: SimpleRootedTree + Sized
 {
     /// Attaches input tree to self by spliting an edge
-    fn graft(&mut self, tree: Self, edge: (Rc<Self::NodeID>, Rc<Self::NodeID>), edge_weights:(Option<Self::EdgeWeight>, Option<Self::EdgeWeight>), graft_edge_weight: Option<Self::EdgeWeight>);
+    fn graft(&mut self, tree: Self, edge: (Self::NodeID, Self::NodeID), edge_weights:(Option<Self::EdgeWeight>, Option<Self::EdgeWeight>), graft_edge_weight: Option<Self::EdgeWeight>);
 
     /// Returns subtree starting at given node, while corresponding nodes from self.
-    fn prune(&mut self, node_id: Rc<Self::NodeID>)-> Self;
+    fn prune(&mut self, node_id: Self::NodeID)-> Self;
 
     /// SPR function
-    fn spr(&mut self, edge1: (Rc<<Self as SimpleRootedTree>::NodeID>, Rc<<Self as SimpleRootedTree>::NodeID>), edge2: (Rc<<Self as SimpleRootedTree>::NodeID>, Rc<<Self as SimpleRootedTree>::NodeID>), edge2_weights: (Option<<Self as SimpleRootedTree>::EdgeWeight>, Option<<Self as SimpleRootedTree>::EdgeWeight>)){
-        let graft_edge_weight = self.get_edge_weight(edge1.0, edge1.1);
-        let pruned_tree = SPR::prune(self, edge1.1);
-        SPR::graft(self, pruned_tree, edge2, edge2_weights, None);
-    }    
+    fn spr(&mut self, edge1: (Self::NodeID, Self::NodeID), edge2: (Self::NodeID, Self::NodeID), edge2_weights: (Option<Self::EdgeWeight>, Option<Self::EdgeWeight>));
+    // {
+    //     let graft_edge_weight = self.get_edge_weight(edge1.0, edge1.1);
+    //     let pruned_tree = SPR::prune(self, edge1.1);
+    //     SPR::graft(self, pruned_tree, edge2, edge2_weights, None);
+    // }    
 }
 
 pub trait NNI
