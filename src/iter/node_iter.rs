ff28150e3dc8f5e9d095519e363d6577f0a64194
sriram98v <sriram.98v@gmail.com>
2024-01-31 14:13:55 -0600

    replaced impl Iterator with impl Intoiterator

--- src/iter/node_iter.rs
+++ src/iter/node_iter.rs
@@ -4,6 +4,6 @@ pub trait Clusters
 where
     Self: SimpleRootedTree + Sized
 {
-    fn get_cluster(&self, node_id: Self::NodeID)-> impl Iterator<Item=Self::NodeID>;
-    fn get_bipartition(&self, edge: (Self::NodeID, Self::NodeID))->(impl Iterator<Item=Self::NodeID>, impl Iterator<Item=Self::NodeID>);
+    fn get_cluster(&self, node_id: Self::NodeID)-> impl IntoIterator<Item=Self::NodeID>;
+    fn get_bipartition(&self, edge: (Self::NodeID, Self::NodeID))->(impl IntoIterator<Item=Self::NodeID>, impl IntoIterator<Item=Self::NodeID>);
 }
--- src/tree/distances.rs
+++ src/tree/distances.rs
@@ -4,8 +4,8 @@ pub trait InterNodeDistances
 where
     Self: WeightedTree + Sized
 {
-    fn leaf_distance_matrix(&self, weighted: bool)->impl Iterator<Item=((Self::NodeID, Self::NodeID), Self::EdgeWeight)>;
-    fn node_distance_matrix(&self, weighted: bool)->impl Iterator<Item=((Self::NodeID, Self::NodeID), Self::EdgeWeight)>;
+    fn leaf_distance_matrix(&self, weighted: bool)->impl IntoIterator<Item=((Self::NodeID, Self::NodeID), Self::EdgeWeight)>;
+    fn node_distance_matrix(&self, weighted: bool)->impl IntoIterator<Item=((Self::NodeID, Self::NodeID), Self::EdgeWeight)>;
     fn distance_from_root(&self, node_id: Self::NodeID, weighted: bool)->Self::EdgeWeight;
     fn distance_from_node(&self, node1: Self::NodeID, node2: Self::NodeID, weighted: bool)->Self::EdgeWeight;
 }
