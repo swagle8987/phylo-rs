1a9953fe473b68d053329311290a2be971bba735
sriram98v <sriram.98v@gmail.com>
2024-02-02 17:25:30 -0600

    updated tests

--- tests/node-tests.rs
+++ tests/node-tests.rs
@@ -1,52 +1,47 @@
 use phylo::node::Node;
-#[cfg(test)]
-mod tests {
+use std::rc::Rc;
+use itertools::Itertools;
+use phylo::node::simple_rnode::RootedTreeNode;
 
-    use std::rc::Rc;
-    use itertools::Itertools;
-    use phylo::node::Node;
-	use phylo::node::simple_rnode::RootedTreeNode;
-
-	#[test]
-    fn test_flip(){
-	let mut n = Node::new(Rc::new(5),true);
-	assert_eq!(n.is_leaf(),true);
-	n.flip();
-	assert_eq!(n.is_leaf(),false);
-	let mut a = Node::new(Rc::new(0),false);
-	assert_eq!(n.is_leaf(),false);
-	a.flip();
-	assert_eq!(a.is_leaf(),true);
-    }
+#[test]
+fn test_flip(){
+let mut n = Node::new(Rc::new(5),true);
+assert_eq!(n.is_leaf(),true);
+n.flip();
+assert_eq!(n.is_leaf(),false);
+let mut a = Node::new(Rc::new(0),false);
+assert_eq!(n.is_leaf(),false);
+a.flip();
+assert_eq!(a.is_leaf(),true);
+}
 
 
-    #[test]
-    fn test_set_id(){
-	let mut n = Node::new(Rc::new(0),true);
-	assert_eq!(n.get_id(),Rc::new(0));
-	n.set_id(Rc::new(10));
-	assert_eq!(n.get_id(),Rc::new(10));
-    }
+#[test]
+fn test_set_id(){
+let mut n = Node::new(Rc::new(0),true);
+assert_eq!(n.get_id(),Rc::new(0));
+n.set_id(Rc::new(10));
+assert_eq!(n.get_id(),Rc::new(10));
+}
 
 
-    #[test]
-    fn test_set_taxa(){
-	let mut n = Node::new(Rc::new(0),true);
-	assert_eq!(n.get_taxa(),None);
-	n.set_taxa(Some(String::from("A")));
-	assert_eq!(n.get_taxa(),Some(String::from("A")));
-    }
+#[test]
+fn test_set_taxa(){
+let mut n = Node::new(Rc::new(0),true);
+assert_eq!(n.get_taxa(),None);
+n.set_taxa(Some(String::from("A")));
+assert_eq!(n.get_taxa(),Some(String::from("A")));
+}
 
-    #[test]
-    fn test_parent_childs(){
-	let mut n = Node::new(0.into(),true);
-	n.add_child(10.into());
-	n.add_child(20.into());
-	assert_eq!(n.get_children().into_iter().collect_vec(), vec![10.into(),20.into()]);
-	n.remove_child(10.into());
-	assert_eq!(n.get_children().into_iter().collect_vec(), vec![20.into()]);
-	n.set_parent(Some(10.into()));
-	assert_eq!(n.get_parent(),Some(10.into()));
-	
-    }
+#[test]
+fn test_parent_childs(){
+let mut n = Node::new(0.into(),true);
+n.add_child(10.into());
+n.add_child(20.into());
+assert_eq!(n.get_children().into_iter().collect_vec(), vec![10.into(),20.into()]);
+n.remove_child(10.into());
+assert_eq!(n.get_children().into_iter().collect_vec(), vec![20.into()]);
+n.set_parent(Some(10.into()));
+assert_eq!(n.get_parent(),Some(10.into()));
+
 }
