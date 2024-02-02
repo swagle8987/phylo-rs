38f309d24c552625baf190ffc5e8b398f3543443
sriram98v <sriram.98v@gmail.com>
2024-02-02 13:46:43 -0600

    updated weighted tree function signature

--- src/tree/weighted.rs
+++ src/tree/weighted.rs
@@ -4,7 +4,7 @@ pub trait WeightedTree
 where
     Self: RootedTree + Sized
 {
-    fn unweight(self)->impl RootedTree;
+    fn unweight(self);
     fn set_edge_weight(&mut self, edge:(Self::NodeID, Self::NodeID), edge_weight:Option<Self::EdgeWeight>);
     fn is_weighted(&self)->bool;
     fn get_edge_weight(&self, parent_id: Self::NodeID, child_id:Self::NodeID)->Option<Self::EdgeWeight>;
