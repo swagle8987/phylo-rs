81c5fddd99f77dabc831afc80847d968a1f0433e
sriram98v <sriram.98v@gmail.com>
2024-02-04 19:34:41 -0600

    updated weighted trait

--- src/tree/weighted.rs
+++ src/tree/weighted.rs
@@ -1,9 +1,13 @@
+use std::{fmt::{Debug, Display}, ops::{Add, AddAssign, Sub, SubAssign}};
+
 use super::RootedTree;
 
 pub trait WeightedTree
 where
     Self: RootedTree + Sized
 {
+    type EdgeWeight: Display + Debug + Clone + Add<Output = Self::EdgeWeight> + AddAssign + Sub<Output = Self::EdgeWeight> + SubAssign;
+
     fn unweight(self);
     fn set_edge_weight(&mut self, edge:(Self::NodeID, Self::NodeID), edge_weight:Option<Self::EdgeWeight>);
     fn is_weighted(&self)->bool;
