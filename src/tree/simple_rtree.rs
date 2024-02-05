a240c1be6f934c19929599b658f622576d528e31
sriram98v <sriram.98v@gmail.com>
2024-02-04 19:35:07 -0600

    added helper methods

--- src/tree/simple_rtree.rs
+++ src/tree/simple_rtree.rs
@@ -1,15 +1,14 @@
 // use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Debug};
 use std::hash::Hash;
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+use itertools::Itertools;
 
 use crate::node::simple_rnode::*;
 
 pub trait RootedTree<RHS=Self> {
     type NodeID: Display + Debug + Hash + Drop + Clone + Ord;
-    type EdgeWeight: Display + Debug + Clone + Add<Output = Self::EdgeWeight> + AddAssign + Sub<Output = Self::EdgeWeight> + SubAssign;
     type Taxa: Debug + Eq + PartialEq + Clone + Ord;
-    type Node: RootedTreeNode<NodeID = Self::NodeID, Taxa = Self::Taxa> + Clone;
+    type Node: RootedTreeNode<NodeID = Self::NodeID, Taxa = Self::Taxa> + Debug + Clone;
 
     fn new(root_id: Self::NodeID)->Self;
 
@@ -41,6 +40,26 @@ pub trait RootedTree<RHS=Self> {
 
     fn get_mrca(&self, node_id_list: &Vec<Self::NodeID>)->Self::NodeID;
 
+    fn set_nodes(&mut self, node_list: impl IntoIterator<Item = Self::Node, IntoIter = impl ExactSizeIterator<Item = Self::Node>>)
+    {
+        for node in node_list.into_iter()
+        {
+            self.set_node(node);
+        }
+    }
+
+    fn split_edge(&mut self, edge: (Self::NodeID, Self::NodeID), node: Self::Node)
+    {
+        let p_id = edge.0;
+        let c_id = edge.1;
+        let n_id = node.get_id();
+        self.set_node(node);
+        self.get_node_mut(p_id.clone()).unwrap().remove_child(c_id.clone());
+        self.set_child(p_id.clone(), n_id.clone());
+        self.set_child(n_id, c_id);
+
+    }
+
     /// Get root node
     fn get_root(&self)->&Self::Node
     {
@@ -49,7 +68,8 @@ pub trait RootedTree<RHS=Self> {
 
     fn set_child(&mut self, parent_id: Self::NodeID, child_id: Self::NodeID)
     {
-        self.get_node_mut(parent_id).unwrap().add_child(child_id);
+        self.get_node_mut(parent_id.clone()).unwrap().add_child(child_id.clone());
+        self.get_node_mut(child_id).unwrap().set_parent(Some(parent_id));
     }
 
     fn get_node_parent(&self, node_id: Self::NodeID)->Option<Self::NodeID>
@@ -69,6 +89,42 @@ pub trait RootedTree<RHS=Self> {
     fn node_degree(&self, node_id: Self::NodeID)->usize
     {
         self.get_node(node_id).unwrap().degree()
+    }
+    fn get_node_depth(&self, node_id: Self::NodeID)->usize
+    {
+        let mut start_id = node_id;
+        let mut depth = 0;
+        while let Some(parent_id) = self.get_node_parent(start_id)
+        {
+            depth+=1;start_id=parent_id;
+        };
+        depth
+    }
+    fn set_node_taxa(&mut self, node_id: Self::NodeID, taxa: Option<<Self as RootedTree<RHS>>::Taxa>)
+    {
+        self.get_node_mut(node_id).unwrap().set_taxa(taxa)
+    }
+    fn flip_node(&mut self, node_id: Self::NodeID)
+    {
+        self.get_node_mut(node_id).unwrap().flip();
+    }
+    fn flip_nodes(&mut self, node_ids: impl Iterator<Item = Self::NodeID>)
+    {
+        for node_id in node_ids
+        {
+            self.flip_node(node_id);
+        }
+    }
+    fn is_binary(&self)->bool
+    {
+        for node in self.get_nodes()
+        {
+            if node.get_children().into_iter().collect_vec().len()>2
+            {
+                return false;
+            }
+        }
+        true
     }
 }
 
