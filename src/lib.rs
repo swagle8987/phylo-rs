639cf0778c7b1432a3d7ad37401bb819762badb6
sriram98v <sriram.98v@gmail.com>
2024-06-13 16:34:49 -0500

    added module prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,12 @@ pub mod node;
 pub mod tree;
 pub mod taxa;
 pub mod iter;
+
+mod prelude{
+    pub use crate::tree::*;
+    pub use crate::node::*;
+    pub use crate::taxa::*;
+    pub use crate::iter::*;
+}
+
+pub use prelude;
