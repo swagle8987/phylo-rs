7098144a04e54f95da2111a05e25bbc05d84f8a0
sriram98v <sriram.98v@gmail.com>
2024-06-13 16:44:30 -0500

    removed import statement

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,9 @@ pub mod tree;
 pub mod taxa;
 pub mod iter;
 
-pub mod prelude{
+mod prelude{
     pub use crate::tree::*;
     pub use crate::node::*;
     pub use crate::taxa::*;
     pub use crate::iter::*;
 }
-
-pub use prelude;
