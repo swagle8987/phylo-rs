f9b3b80b818741e230a8831d3f686defd8976254
sriram98v <sriram.98v@gmail.com>
2024-06-21 12:13:21 -0500

    updated prelude to include only traits

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ pub mod tree;
 pub mod taxa;
 pub mod iter;
 
-mod prelude{
-    pub use crate::tree::*;
-    pub use crate::node::*;
-    pub use crate::taxa::*;
-    pub use crate::iter::*;
+pub mod prelude{
+    #[doc(no_inline)]
+    pub use crate::tree::simple_rtree::*;
+    #[doc(no_inline)]
+    pub use crate::node::simple_rnode::*;
+    #[doc(no_inline)]
+    pub use crate::iter::node_iter::*;
 }
