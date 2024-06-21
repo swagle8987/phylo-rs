df1080528fd33cde56c73e347a5f39dceef84c52
sriram98v <sriram.98v@gmail.com>
2024-06-21 12:15:07 -0500

    added ops, io and distances to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ pub mod prelude{
     #[doc(no_inline)]
     pub use crate::tree::simple_rtree::*;
     #[doc(no_inline)]
+    pub use crate::tree::io::*;
+    #[doc(no_inline)]
+    pub use crate::tree::ops::*;
+    #[doc(no_inline)]
+    pub use crate::tree::distances::*;
+    #[doc(no_inline)]
     pub use crate::node::simple_rnode::*;
     #[doc(no_inline)]
     pub use crate::iter::node_iter::*;
