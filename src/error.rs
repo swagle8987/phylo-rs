ee0b8e977367f5c29b7026534b04b175a6a940d3
sriram98v <sriram.98v@gmail.com>
2024-07-24 14:50:51 -0500

    added more error types for mutation and query

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter, Result};
 use std::error::Error;
 
-/// A type to for errors when parsing newick strings
+/// A type for errors when parsing newick strings
 #[derive(Debug)]
 pub struct NewickErr(pub String);
 
@@ -13,7 +13,7 @@ impl Display for NewickErr {
 
 impl Error for NewickErr {}
 
-/// A type to for errors when parsing Nexus files
+/// A type for errors when parsing Nexus files
 #[derive(Debug)]
 pub struct NexusErr(pub String);
 
@@ -24,3 +24,51 @@ impl Display for NexusErr {
 }
 
 impl Error for NexusErr {}
+
+/// A type for errors when mutating trees
+#[derive(Debug)]
+pub struct TreeMutationErr(pub String);
+
+impl Display for TreeMutationErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for TreeMutationErr {}
+
+/// A type for errors when querying trees
+#[derive(Debug)]
+pub struct TreeQueryErr(pub String);
+
+impl Display for TreeQueryErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for TreeQueryErr {}
+
+/// A type for errors when mutating Nodes
+#[derive(Debug)]
+pub struct NodeMutationErr(pub String);
+
+impl Display for NodeMutationErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for NodeMutationErr {}
+
+/// A type for errors when querying Nodes
+#[derive(Debug)]
+pub struct NodeQueryErr(pub String);
+
+impl Display for NodeQueryErr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for NodeQueryErr {}
