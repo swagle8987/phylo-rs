2e5d56f2ab9953d56fa4f30c0cc79696e01dffd2
sriram98v <sriram.98v@gmail.com>
2025-02-16 16:39:05 -0600

    updated pairwise distances example

--- examples/pairwise-distances.rs
+++ examples/pairwise-distances.rs
@@ -4,20 +4,17 @@ use rayon::prelude::*;
 use itertools::Itertools;
 use std::fs::read_to_string;
 use phylo::prelude::*;
-use indicatif::{ProgressIterator, ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressStyle};
 use phylo::tree::DemoTree;
 
 #[cfg(feature = "parallel")]
 fn main() {
-    let trees = (1..11).progress().map(|x| read_to_string(format!("examples/pairwise-distances/r{x}-preprocessed.trees"))
+    let trees = read_to_string(format!("examples/pairwise-distances/sample-trees.trees"))
             .unwrap()
             .lines()
             .enumerate()
-            .map(|(y,z)| (x,y,DemoTree::from_newick(z.as_bytes()).unwrap()))
-            .collect_vec()
-        )
-        .flatten()
-        .collect_vec();
+            .map(|(y,z)| (y,DemoTree::from_newick(z.as_bytes()).unwrap()))
+            .collect_vec();
 
     let bar = ProgressBar::new((trees.len()*(trees.len()-1)/2) as u64);
     bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg} [eta: {eta}]")
@@ -25,7 +22,7 @@ fn main() {
     .progress_chars("##-"));
 
     trees.iter().combinations(2).par_bridge().map(|v| (v[0], v[1])).for_each(|(x,y)| {
-        let out = format!("{}-{}-{}-{}-{}\n", x.0, y.0, x.1, y.1, x.2.ca(&y.2));
+        let out = format!("{}-{}-{}\n", x.0, y.0, x.1.ca(&y.1));
         println!("{}", out);
         bar.inc(1);
     });
@@ -34,16 +31,12 @@ fn main() {
 
 #[cfg(not(feature = "parallel"))]
 fn main() {
-    let trees = (1..11).progress().map(|x| read_to_string(format!("examples/pairwise-distances/r{x}-preprocessed.trees"))
+    let trees = read_to_string(format!("examples/pairwise-distances/sample-trees.trees"))
             .unwrap()
             .lines()
             .enumerate()
-            .map(|(y,z)| (x,y,DemoTree::from_newick(z.as_bytes()).unwrap()))
-            .collect_vec()
-        )
-        .flatten()
-        .collect_vec();
-
+            .map(|(y,z)| (y,DemoTree::from_newick(z.as_bytes()).unwrap()))
+            .collect_vec();
 
     let bar = ProgressBar::new((trees.len()*(trees.len()-1)/2) as u64);
     bar.set_style(ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg} [eta: {eta}]")
@@ -51,7 +44,7 @@ fn main() {
     .progress_chars("##-"));
 
     trees.iter().combinations(2).map(|v| (v[0], v[1])).for_each(|(x,y)| {
-        let out = format!("{}-{}-{}-{}-{}\n", x.0, y.0, x.1, y.1, x.2.ca(&y.2));
+        let out = format!("{}-{}-{}\n", x.0, y.0, x.1.ca(&y.1));
         println!("{}", out);
         bar.inc(1);
     });
